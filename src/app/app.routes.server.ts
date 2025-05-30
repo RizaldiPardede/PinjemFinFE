b679cdd189114d556fc228df188132ff9ad1c532
RizaldiPardede <rizaldipardede@gmail.com>
2025-05-30 16:16:02 +0700

    Final Front end 6

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,13 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
+  {
+    path: 'resetpassword/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams() {
+      return Promise.resolve([]);
+    }
+     // ✅ fix ini
+  },
 ];
