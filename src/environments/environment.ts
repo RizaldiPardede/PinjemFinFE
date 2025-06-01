b8d3b89570d81ad0aee0b0010326a50e78aabb2e
RizaldiPardede <rizaldipardede@gmail.com>
2025-06-01 21:34:22 +0700

    ganti url backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:8080/api/'
-    apiUrl: 'http://34.30.17.63/api/'
+    apiUrl: 'http://35.202.72.129/api/'
   };
